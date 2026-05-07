759467671ababdbfce8935ade0ec3e664e7fd452
Franco Aguiar <131722274+FrancoAguiar@users.noreply.github.com>
2026-05-07 15:03:30 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,8 @@
-const CACHE_NAME = 'sticker-cup-2026-v1';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './albumData.js'
-];
-
+const CACHE_NAME = 'sticker-cup-2026-v7';
+const ASSETS = ['./', './index.html', './albumData.js'];
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
-  self.clients.claim();
-});
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
